15332266ae2b355f0b56b6a2a0dce12e59891bf9
niuxinyu <1096196462@qq.com>
2020-04-28 19:10:59 +0800

    beautifiy pc index

--- src/pages/pc/index/index.js
+++ src/pages/pc/index/index.js
@@ -7,11 +7,8 @@ window.onload = () => {
   // 引入并使用layui相关组件
   layui.use([ 'layer', 'element'], function() {
     var layer = layui.layer //弹层
-    ,element = layui.element; //元素操作 等等...
-
-    /*layer弹出一个示例*/
     layer.msg('Hello World');
-  });
+  })
   
   //异步操作
   getIndex('http://sditer.com:8088/mall/api/mallListByCatApi.php?catId=565').then(res => {
